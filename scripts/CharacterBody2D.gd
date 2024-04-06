f07bc210725f4066d7caefad9c54a90018043a99
Snappydoodle <96540210+Snappydoodle@users.noreply.github.com>
2024-04-05 17:20:31 -0700

    added a bunch of stuff

--- scripts/CharacterBody2D.gd
+++ scripts/CharacterBody2D.gd
@@ -12,7 +12,9 @@ signal goalTouched
 @export var wallBounciness : float = .065
 @export var minSpringBounciness : float = 500
 @export var clickSpeed : float = .2
-#@export var extraAirResistanceThreshold : float = 750
+@export var spriteScale : int = 2
+@export var spriteStretch : float = 1
+#@export var extraAirResistance`Threshold : float = 750
 
  #Your motherings ssss read
 
@@ -20,6 +22,9 @@ signal goalTouched
 @onready var timeElapsed = get_node("/root/Node2D").timeElapsed
 @onready var startTimer = get_node("/root/Node2D").startTimer
 @onready var Filepath = get_node("/root/Node2D").scene_file_path
+
+var SHOTGUN_PARTICLES = preload("res://scripts/shotgun_particles.tscn")
+var BULLET = preload("res://scripts/Bullet.tscn")
 
 var double_jumps : int = 0
 var velocityLaunch = Vector2(0,0)
@@ -31,6 +36,10 @@ var mousePosition = Vector2(0,0)
 var mousePlayerAngle : float = 0 #the angle between the mouse and the player
 
 @onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
+@onready var shoot_cooldown_timer : Timer = $ShootCooldownTimer
+@onready var idle_timer : Timer = $IdleTimer
+@onready var gun_sprite : AnimatedSprite2D = $Gun/GunSprite
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var animation_locked : bool = false
@@ -41,6 +50,13 @@ var clickBuffer : bool = false
 var canClick : bool = true
 var deltaGlobal : float = 0.00
 
+var lastIdleRandom : int = -1
+
+var gunReloaded : bool = false
+
+func _ready():
+	animated_sprite.play("idle")
+	animation_player.play("idle")
 func _physics_process(delta):
 	deltaGlobal = delta
 	# Add the gravity.
@@ -52,11 +68,14 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 		if is_double_jumping == false:
 			if velocity.y > 0:
-				animated_sprite.play("fall")
+				#animated_sprite.play("fall")
+				pass
 			else:
-				animated_sprite.play("jump")
+				#animated_sprite.play("jump")
+				pass
 		else:
-			animated_sprite.play("double jump")
+			#animated_sprite.play("double jump")
+			pass
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump"):
@@ -68,7 +87,8 @@ func _physics_process(delta):
 			is_double_jumping = true
 			velocity.y = double_jump_velocity
 			double_jumps -= 1
-			
+	
+	updateMousePosition()
 	if Input.is_action_just_pressed("mouseLeftClick"):
 		mouseLeftClick()
 
@@ -101,28 +121,87 @@ func _physics_process(delta):
 	
 	update_animation()
 	update_facing_direction()
+	idleAnimation()
+	updateSpriteStretching()
 	move_and_slide()
-	
+	roundVelocityLaunch()
+	reloadGun()
 	
 	#velocity.y = velocity.y + velocityLaunch.y
+func reloadGun():
+	if is_on_floor() and !(gunReloaded):
+		gun_sprite.play("shotgun_reload")
+		$Gun/ShotgunReload.play()
+		gunReloaded = true
+func roundVelocityLaunch():
+	if velocityLaunch.x >= -0.01 and velocityLaunch.x <= 0.01:
+		velocityLaunch.x = 0
 	
 func updateTimer():
 	if startTimer:
 		get_node("/root/Node2D").timeElapsed = timeElapsed
 		timeElapsed += deltaGlobal
 
+func updateSpriteStretching():
+	animated_sprite.scale.x = (1 / spriteStretch) * spriteScale
+	animated_sprite.scale.y = spriteStretch * spriteScale
+	animated_sprite.position.y = -10 * spriteStretch + 10
+	#print(animation_player.current_animation)
+	if animation_player.current_animation == "air":
+		var velocityRatio = abs(velocity.y + 1) / abs(velocityLaunch.x + 1)
+		if velocityRatio >= PI / 2:
+			velocityRatio = PI / 2
+		spriteStretch = .05 * cos(2 * (velocityRatio - (PI / 2))) + 1
+		#print(str(velocity.y + 1) + " / " + str(velocityLaunch.x + 1) + " = " + str(velocityRatio))
+	
 func update_animation():
 	if not animation_locked:
 		if direction.x != 0:
-			animated_sprite.play("run")
+			#animated_sprite.play("run")
+			pass
 		else:
-			animated_sprite.play("idle")
+			#animated_sprite.play("idle")
+			pass
 
+func idleAnimation():
+	if velocity.y == 0 and velocityLaunch.x == 0:
+		animation_player.play("idle")
+		if idle_timer.time_left == 0:
+			randomize()
+			idle_timer.start(randf_range(4, 6))
+	if !(velocity.y == 0 and velocityLaunch.x == 0):
+		idle_timer.stop()
+
+func _on_idle_timer_timeout():
+	if velocity.y == 0 and velocityLaunch.x == 0:
+		randomize()
+		var idleRandom = randi_range(1, 3)
+		while idleRandom == lastIdleRandom:
+			randomize()
+			idleRandom = randi_range(1, 3)
+		if idleRandom == 1:
+			animated_sprite.play("blink")
+		elif idleRandom == 2:
+			animated_sprite.play("shine")
+		else:
+			animated_sprite.play("glitch")
+		lastIdleRandom = idleRandom
+		idle_timer.start(randf_range(8, 10))
+	
 func update_facing_direction():
-	if direction.x > 0:
+	#if velocityLaunch.x > 0: #OLD VERSION
+	#	animated_sprite.flip_h = false
+	#elif velocityLaunch.x < 0:
+	#	animated_sprite.flip_h = true
+	if abs(rad_to_deg(mousePlayerAngle)) > 90:
 		animated_sprite.flip_h = false
-	elif direction.x < 0:
+	else:
 		animated_sprite.flip_h = true
+	if $Gun.rotation > PI / 2 and $Gun.rotation < ((3 * PI) / 2):
+		$Gun.scale.y = -1
+	else:
+		$Gun.scale.y = 1
+	pass 
 
 func jump():
 	velocity.y = jump_velocity
@@ -135,30 +214,51 @@ func addWallBounce():
 func mouseLeftClick():
 	if canClick:
 		launchPlayer()
-		$Timer.start(clickSpeed)
+		shoot_cooldown_timer.start(clickSpeed)
 		canClick = false
 	else:
 		clickBuffer = true
 
-func _on_timer_timeout():
+func _on_ShootCooldownTimer_timeout():
 	if clickBuffer:
 		clickBuffer = false
-		$Timer.start(clickSpeed)
+		shoot_cooldown_timer.start(clickSpeed)
 		launchPlayer()
 	else:
 		canClick = true
 	pass # Replace with function body.
 
 func launchPlayer():
-	updateMousePosition()
-	mousePlayerAngle = mousePosition.angle_to_point(position)
-		
+	animated_sprite.play("shoot")
+	gun_sprite.play("shotgun_shoot")
+	animation_player.play("air")
+	$Gun/ShotgunShoot.play()
+	$Gun/ShotgunReload.stop()
+	gunReloaded = false
+	
+	var shotgun_particles = SHOTGUN_PARTICLES.instantiate()
+	add_child(shotgun_particles)
+	shotgun_particles.global_position = Vector2(position.x - cos(mousePlayerAngle) * 37.5, position.y - sin(mousePlayerAngle) * 37.5)
+	shotgun_particles.rotation = $Gun.rotation
+	#print(str(get_node(shotgun_particles)))
+	
+	shootBullets(4, 15)
+	
 	velocityLaunch.x = launchMultiplier * cos(mousePlayerAngle)
 	velocity.y = launchMultiplier * sin(mousePlayerAngle) * .75
+	
 	startTimer = true
+	
 	bulletsFired += 1
 	get_node("/root/Node2D").bulletsFired = bulletsFired
 	
+func shootBullets(amount, spread):
+	for i in range(amount):
+		randomize()
+		var bullet = BULLET.instantiate()
+		add_sibling(bullet)
+		bullet.global_position = Vector2(position.x - cos(mousePlayerAngle) * 37.5, position.y - sin(mousePlayerAngle) * 37.5)
+		bullet.rotation = $Gun.rotation + deg_to_rad(randf_range(spread * -1, spread))
 func addAirResistance():
 	airResistanceB()
 	#if abs(velocityLaunch.x) >= extraAirResistanceThreshold:
@@ -184,7 +284,8 @@ func airResistanceB():
 		
 func updateMousePosition():
 	mousePosition = get_global_mouse_position()
-	
+	mousePlayerAngle = mousePosition.angle_to_point(position)
+	$Gun.rotation = mousePlayerAngle + PI
 
 func _on_death_detection_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	if body is TileMap:
@@ -201,6 +302,7 @@ func processTilemapCollision(body, body_rid):
 	var tileData = body.get_cell_tile_data(0, collidedTileCoords)
 	#print(tileData)
 	var interactableType = tileData.get_custom_data_by_layer_id(0) #returns value of custom metadata
+	#print("player: " + interactableType)
 	if interactableType == "Deadly": 
 		death()
 	elif interactableType == "SpringRight":
@@ -229,8 +331,12 @@ func death():
 func _on_interactable_detection_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
 	if area.is_in_group("GoalZone"):
 		goalTouched.emit()
-		print(area_rid)
 	pass # Replace with function body.
 
+func _on_animated_sprite_2d_animation_finished():
+	animated_sprite.play("idle")
+	pass # Replace with function body.
 
-
+func _on_gun_sprite_animation_finished():
+	gun_sprite.play("shotgun_idle")
+	pass # Replace with function body.
